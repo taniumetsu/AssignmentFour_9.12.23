a0f406a62b9a217ce5e4a494377f84659e89d821
Tani Umetsu <taniu@hawaii.edu>
2023-09-12 13:29:42 -1000

    Completed Parts 1-4

--- if else statement part 1.playground/Contents.swift
+++ if else statement part 1.playground/Contents.swift
@@ -1,3 +1,36 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+//Part 2 of Assmt 4
+
+var goodGrade = "I've been doing all my work"
+
+//create first if statement
+//this is the conditional statement
+if goodGrade == "I've been doing all my work"
+{
+    //if block
+    print("I should be getting an A at this point")
+    //only prints if the condition is true (if goodgrade=...)
+    
+}
+//Part 3
+else
+{
+    //else block
+    print("I need to work harder in this class")
+}
+
+//Part 4
+var gradeA = true
+
+if gradeA == true
+{
+    print("I'm great at this class")
+}
+
+else
+{
+    print("I'm kind of bad at this")
+}
